7ffc92ae4e0c0cff546eb45e86af4ea8ab89bbd3
Denys07748 <denis07748@gmail.com>
2023-04-18 18:24:57 +0300

    додає PropTypes на ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -18,6 +18,8 @@ ContactList.propTypes = {
     contacts: PropTypes.arrayOf(
         PropTypes.shape({
           id: PropTypes.string.isRequired,
+          name: PropTypes.string.isRequired,
+          number: PropTypes.string.isRequired,
         }),
     ).isRequired,
     onDeleteContact: PropTypes.func.isRequired,
